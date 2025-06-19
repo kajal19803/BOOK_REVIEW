b0e445a6dcb3a52cfec6d44f68905919930544d5
Kajal Verma <kajalverma6263@gmail.com>
2025-06-20 00:55:52 +0530

    fix: set base path for correct asset URLs

--- OneDrive/Desktop/BOOK_REVIEW/Frontend/vite.config.js
+++ OneDrive/Desktop/BOOK_REVIEW/Frontend/vite.config.js
@@ -1,17 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
   server: {
     proxy: {
+      
       '/api': {
-        target: 'http://localhost:5000',  
+        target: process.env.VITE_API_URL ,
         changeOrigin: true,
         secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '/api'),
       },
     },
   },
-})
+});
+
 
