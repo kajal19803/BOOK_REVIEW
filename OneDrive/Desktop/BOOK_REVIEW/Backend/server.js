911de55229b9ba2312854379157382394d04c84a
Kajal Verma <kajalverma6263@gmail.com>
2025-06-20 00:04:41 +0530

    add redirects file in frontend in public folder

--- OneDrive/Desktop/BOOK_REVIEW/Backend/server.js
+++ OneDrive/Desktop/BOOK_REVIEW/Backend/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const dotenv = require("dotenv");
+const dotenv = require ("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
 dotenv.config();
@@ -16,6 +16,4 @@ app.use("/api/reviews", require("./routes/reviewRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+app.listen(PORT, () => {});
