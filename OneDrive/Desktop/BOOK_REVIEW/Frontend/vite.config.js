1b77cff7047fd80b6fde02e18c9486be8a5651ae
Kajal Verma <kajalverma6263@gmail.com>
2025-06-20 14:04:38 +0530

    ensure refresh

--- OneDrive/Desktop/BOOK_REVIEW/Frontend/vite.config.js
+++ OneDrive/Desktop/BOOK_REVIEW/Frontend/vite.config.js
@@ -2,19 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',           
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '/api'),
-      },
-    },
+  build: {
+    outDir: 'build',
   },
 });
+
 
 
 
